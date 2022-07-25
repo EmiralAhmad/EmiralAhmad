dbde66d48b75ff96ea7c78b4881e37ffe615078d
EmiralAhmad <emiralbahari12@gmail.com>
2022-07-25 16:53:08 +0700

    ini CH5

---
+++ Challenge/CH5.js
@@ -0,0 +1,11 @@
+function stringManipulation(word) {
+
+    if (word == 'ayam') {
+        return 'ayam';
+    } else {
+        return 'bebek' + 'nyo' ;
+}
+}
+
+console.log(stringManipulation('ayam'));
+console.log(stringManipulation('bebek'));
